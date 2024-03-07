2e910eecbea7c90dcae0134b815011e3153cef17
danielvilla04 <danielvillalobos0408@gmail.com>
2024-03-07 11:30:41 -0600

    Creación de projecto con SpringBoot

---
+++ lbd_project/src/main/java/com/lbdproject/lbd_project/LbdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lbdproject.lbd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbdProjectApplication.class, args);
+	}
+
+}
---
+++ lbd_project/src/test/java/com/lbdproject/lbd_project/LbdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbdproject.lbd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
